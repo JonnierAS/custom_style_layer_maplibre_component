561a445f5e4b8c0f0843739bc584e9d1eb206199
Jonnier <jonniersalaverria@gmail.com>
2024-03-19 10:43:02 -0500

    Anexo de componente para seleccionar las capas

--- front/src/components/layout/contentLayout/style_layers/LayerStyle/LayerStyle.jsx
+++ front/src/components/layout/contentLayout/style_layers/LayerStyle/LayerStyle.jsx
@@ -2,6 +2,7 @@ import {ColorWheelIcon} from "@radix-ui/react-icons";
 
 import { useLocalState } from "../../../../context/CleanLocalState";
 import HexAlphaColor from "../components/HexAlphaColor";
+import SelectLayers from "../components/SelectLayers";
 
 const styleInput = "absolute w-28 top-[-10px] left-28 py-1 px-2 text-sm rounded outline-none focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:ring-purple-500 dark:focus-visible:ring-purple-500 dark:focus-visible:ring-offset-gray-900 border border-gray-200 dark:border-gray-500 shadow-sm focus-visible:border-gray-200 dark:focus-visible:border-gray-300 hover:border-gray-300 dark:hover:border-gray-300 block dark:bg-transparent dark:text-gray-100"
 
@@ -24,6 +25,9 @@ function LayerStyle() {
       };
   return (
     <div className="text-center relative top-5">
+      <div className="flex justify-center">
+        <SelectLayers />
+      </div>
       <div className="flex-col">
         <div className="flex w-40 justify-between relative left-3 top-5">
             <label className="font-medium">Color base:</label>
