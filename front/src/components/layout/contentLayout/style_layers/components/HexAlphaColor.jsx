7ba9360afc90121cdd35c85ba6c20f33e847ed1a
Jonnier <jonniersalaverria@gmail.com>
2024-03-19 09:12:51 -0500

    Uso del estado con las propiedades del color de linea o base

--- front/src/components/layout/contentLayout/style_layers/components/HexAlphaColor.jsx
+++ front/src/components/layout/contentLayout/style_layers/components/HexAlphaColor.jsx
@@ -3,12 +3,12 @@ import {
     HexAlphaColorPicker,
   } from "react-colorful";
 
-export default function HexAlphaColor({colorState, handleColorChange, setOpenModalChangeColor}) {
+export default function HexAlphaColor({layersPropertyStyle, handleColorChange, setOpenModalChangeColor}) {
   return (
     <div className="absolute z-[1000] bg-white w-[160px] h-[220px] rounded-lg p-1 flex items-center justify-center text-xs right-5 border-2">
             <div className="space-y-1 flex flex-col p-1">
               <HexAlphaColorPicker
-                color={colorState || "#6e548c"}
+                color={layersPropertyStyle.colorBase || layersPropertyStyle.lineColor || "#6e548c"}
                 onChange={handleColorChange}
                 className="color-demographic"
                 style={{ height: "140px", width: "140px" }}
@@ -17,12 +17,12 @@ export default function HexAlphaColor({colorState, handleColorChange, setOpenMod
                 className={`p-3 rounded border-2 font-sans font-medium h-6 w-full`}
                 prefixed
                 alpha
-                color={colorState || "#6e548c"}
+                color={layersPropertyStyle.colorBase || layersPropertyStyle.lineColor || "#6e548c"}
                 onChange={handleColorChange}
               />
               <button
                 onClick={() => {
-                  setOpenModalChangeColor(false);
+                  setOpenModalChangeColor({state: false, type: ""});
                 }}
                 className="w-full p-1 border-2 font-medium rounded hover:bg-gray-300 "
               >
