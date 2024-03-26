d1d4157eec1ce97268349e788ce53cad4f9d6c6c
Jonnier <jonniersalaverria@gmail.com>
2024-03-26 12:38:00 -0500

    se creo estado para las propiedades de los iconos y texto de los iconos

--- front/src/components/context/CleanLocalState.jsx
+++ front/src/components/context/CleanLocalState.jsx
@@ -16,6 +16,30 @@ export const LocalStateProvider = ({ children }) => {
     minZoomRadius: 0.5,
     maxZoomRadius: 24
   })
+  const [layerIconProperties, setLayerIconProperties] = useState({
+    color: "black",
+    icon: "marker",
+    opacity: 1,
+    haloBlur: 0,
+    haloColor: "white",
+    haloWidth: 0,
+    overlap: "always",
+    rotate: 0,
+    pitchAlignment: "auto",
+    textColor: "black",
+    textOverlap: "never",
+    textRotate: 0,
+    textSize: 16,
+    textOffsetX: 0,
+    textOffsetY: 0,
+    textAnchor: "center",
+    textOptional: true,
+    textTransform: "none",
+    textOpacity: 1,
+    adaptOnZoom: false,
+    minZoomIconSize: 0.1,
+    maxZoomIconSize: 1,
+  });
   const [openModalChangeColor, setOpenModalChangeColor] = useState({state: false, type: ""});
   
   const [applyTransition , setApplyTransition] = useState(false)
@@ -27,7 +51,8 @@ export const LocalStateProvider = ({ children }) => {
         openModalChangeColor, setOpenModalChangeColor,
         showPanel, setShowPanel,
         applyTransition , setApplyTransition,
-        layersPropertyStyle, setLayerPropertyStyle
+        layersPropertyStyle, setLayerPropertyStyle,
+        layerIconProperties, setLayerIconProperties
       }}
     >
       {children}
