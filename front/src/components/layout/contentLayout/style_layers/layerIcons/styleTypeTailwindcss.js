ef2996ef018dcb905d769013fe4f8fa62faec730
Jonnier <jonniersalaverria@gmail.com>
2024-03-26 12:37:14 -0500

    creacion de archivo para las variables de los estilos

---
+++ front/src/components/layout/contentLayout/style_layers/layerIcons/styleTypeTailwindcss.js
@@ -0,0 +1,3 @@
+export const styleInput = "absolute w-28 top-[-10px] left-32 py-1 px-2 text-sm rounded outline-none focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:ring-purple-500 dark:focus-visible:ring-purple-500 dark:focus-visible:ring-offset-gray-900 border border-gray-200 dark:border-gray-500 shadow-sm focus-visible:border-gray-200 dark:focus-visible:border-gray-300 hover:border-gray-300 dark:hover:border-gray-300 block dark:bg-transparent dark:text-gray-100";
+export const styleLabel = "font-medium"
+export const styleDivContainer = "flex w-40 justify-between relative left-3"
