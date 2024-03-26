390c52fb366643b2c17e8f25c4f2f9534e79ee02
Jonnier <jonniersalaverria@gmail.com>
2024-03-26 15:34:08 -0500

    Se agrego manejador para el layaout y el paint

--- front/src/components/layout/contentLayout/style_layers/services/symbol.js
+++ front/src/components/layout/contentLayout/style_layers/services/symbol.js
@@ -4,3 +4,15 @@ export function loadSymbols(map, icon) {
     map.addImage(icon, image, { sdf: true });
   });
 }
+
+export function setLayoutProperties(map, layerId, properties) {
+  for (const key in properties) {
+    map.setLayoutProperty(layerId, key, properties[key]);
+  }
+}
+
+export function setPaintProperties(map, layerId, properties) {
+  for (const key in properties) {
+    map.setPaintProperty(layerId, key, properties[key]);
+  }
+}
