34deb959274155b3ee68c3b069962417a6a1f3ee
Jonnier <jonniersalaverria@gmail.com>
2024-03-27 10:00:31 -0500

    se creo contenedeor para almacenar los esxtilos del icono y texto

---
+++ front/src/components/layout/contentLayout/style_layers/layerIcons/LayerIconContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayerIcons from './components/LayerIcons'
+import TextStyleIcon from './components/TextStyleIcon'
+
+export default function LayerIconContainer() {
+  return (
+    <div>
+        <LayerIcons />
+        <TextStyleIcon />
+    </div>
+  )
+}
