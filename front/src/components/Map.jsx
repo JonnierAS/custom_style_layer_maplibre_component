12f866aa010a1d69756b5c4fdf1afb47eccc9194
Jonnier <jonniersalaverria@gmail.com>
2024-03-28 11:57:56 -0500

    condicional de renderizado

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -143,6 +143,7 @@ export default function MapContainer() {
               }/gwc/service/tms/1.0.0/azzorti_vt:${layerName}@EPSG%3A900913@pbf/{z}/{x}/{y}.pbf`,
             ]}
           >
+            {!layerIconProperties.showIcon && 
             <Layer
               type="circle"
               source={layerName}
@@ -162,37 +163,39 @@ export default function MapContainer() {
                 "circle-pitch-alignment": layersPropertyStyle.pitchAligment,
                 "circle-pitch-scale": layersPropertyStyle.pitchScale
               }}
-            />
-            <Layer
+            />}
+            {layerIconProperties.showIcon && 
+              <Layer
               type="symbol"
-              source={layerName}
-              id={`${layerName}-icon`}
-              key={`${layerName}-icon`}
-              source-layer={layerName}
-              filter={["==", "$type", "Point"]}
-              layout={{
-                "icon-image": layerIconProperties.icon,
-                "icon-overlap": layerIconProperties.overlap,
-                "icon-rotate": layerIconProperties.rotate,
-                "icon-pitch-alignment": layerIconProperties.pitchAlignment,
-                "icon-size": layerIconProperties.size,
-                "text-field": layerIconProperties.textOptional ? `{${layerIconProperties.textField}}`: null,
-                "text-font": ["Open Sans Regular"],
-                "text-size": layerIconProperties.textSize,
-                "text-transform": layerIconProperties.textTransform,
-                "text-offset":  [layerIconProperties.textOffsetX,layerIconProperties.textOffsetY],
-                "text-allow-overlap": layerIconProperties.textOverlap,
-                "text-rotate": layerIconProperties.textRotate,
-                "text-anchor": layerIconProperties.textAnchor,
-                "text-optional": layerIconProperties.textOptional
-              }}
-              paint={{
-                "icon-color": layerIconProperties.color || "#6e548c",
-                "icon-halo-width": layerIconProperties.haloWidth|| 1,
-                "icon-halo-color": layerIconProperties.haloColor || "#000000",
-                "text-color": layerIconProperties.textColor,
-              }}
-            />
+                source={layerName}
+                id={`${layerName}-icon`}
+                key={`${layerName}-icon`}
+                source-layer={layerName}
+                filter={["==", "$type", "Point"]}
+                layout={{
+                  "icon-image": layerIconProperties.icon,
+                  "icon-overlap": layerIconProperties.overlap,
+                  "icon-rotate": layerIconProperties.rotate,
+                  "icon-pitch-alignment": layerIconProperties.pitchAlignment,
+                  "icon-size": layerIconProperties.size,
+                  "text-field": layerIconProperties.textOptional ? `{${layerIconProperties.textField}}`: null,
+                  "text-font": ["Open Sans Regular"],
+                  "text-size": layerIconProperties.textSize,
+                  "text-transform": layerIconProperties.textTransform,
+                  "text-offset":  [layerIconProperties.textOffsetX,layerIconProperties.textOffsetY],
+                  "text-allow-overlap": layerIconProperties.textOverlap,
+                  "text-rotate": layerIconProperties.textRotate,
+                  "text-anchor": layerIconProperties.textAnchor,
+                  "text-optional": layerIconProperties.textOptional
+                }}
+                paint={{
+                  "icon-color": layerIconProperties.color || "#6e548c",
+                  "icon-halo-width": layerIconProperties.haloWidth|| 1,
+                  "icon-halo-color": layerIconProperties.haloColor || "#000000",
+                  "text-color": layerIconProperties.textColor,
+                }}
+              />
+            }
           </Source>
         </>
         ): null}
