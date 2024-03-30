c1832bba19b63fbfb4b7c722a589969b9b79661c
Jonnier <jonniersalaverria@gmail.com>
2024-03-30 10:15:20 -0500

    anexo de prop para diferenciar de donde se renderizan los datos

--- front/src/components/Map.jsx
+++ front/src/components/Map.jsx
@@ -54,7 +54,7 @@ export default function MapContainer() {
         panelWidth={panelWidth}
         setPanelWidth={setPanelWidth}
         setShowPanel={setShowPanel}
-        showPanel={showPanel}
+        showPanel={showPanel} mapFeature={"mapLibre"}
       />
       <Map
         ref={mapRef}
