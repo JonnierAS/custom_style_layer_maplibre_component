0dfb875c5ff6769fad8460573ab8dc92eb5467d8
Jonnier <jonniersalaverria@gmail.com>
2024-04-02 12:32:46 -0500

    Creacion de funcion para obtener los estilo de una capa

--- front/src/components/layout/contentLayout/style_layers/services/styleManage.js
+++ front/src/components/layout/contentLayout/style_layers/services/styleManage.js
@@ -2,6 +2,16 @@ import axios from "axios"
 export const saveNewStyle = async(style)=>{
     try {
         const response = await axios.post(`${import.meta.env.VITE_API_V1_NODE}/capa/add-style-layer`, {style: style}); 
+        alert("Estilo guardado con exito!")
+        return response
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getStyleOfLayerSelected = async(name)=>{
+    try {
+        const response = await axios.get(`${import.meta.env.VITE_API_V1_NODE}/capa/get-styles-layers/${name}`); 
         return response
     } catch (error) {
         console.log(error);
