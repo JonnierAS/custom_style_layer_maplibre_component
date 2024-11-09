37c7c94581e98185ca24e50870345e34693870f4
JonnierAS <jonniersalaverria@gmail.com>
2024-11-09 15:07:36 -0500

    delete deckGl

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -1,17 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import MapContainer from "./components/Map";
-import MapContainerDeckGl from "./components/MapDeckGl";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MapContainer />
   },
-  // {
-  //   path: '/',
-  //   element: <MapContainerDeckGl />
-  // },
-
- 
 ])
 export default router
